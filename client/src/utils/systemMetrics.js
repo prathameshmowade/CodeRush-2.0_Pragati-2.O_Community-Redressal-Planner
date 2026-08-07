4447e483d7c9be2a76c8d301c9a1e71cc58fbd0d
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 21:43:47 +0530

    add banner logic

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 179;
-export const COMMIT_MSG = "sync model data";
-export const COMMIT_TIME = "2026-08-07 21:39:16 +0530";
-export const BUILD_HASH = "0x536636";
+export const BUILD_STEP = 180;
+export const COMMIT_MSG = "add banner logic";
+export const COMMIT_TIME = "2026-08-07 21:43:47 +0530";
+export const BUILD_HASH = "0x8c262a";
