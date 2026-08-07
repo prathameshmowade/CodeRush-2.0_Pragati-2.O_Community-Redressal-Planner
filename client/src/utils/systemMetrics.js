b5bc9256df7c568b653f2f57ced6a71b0efeec8e
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 13:56:25 +0530

    optimize helper code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 60;
-export const COMMIT_MSG = "adjust drawer speed";
-export const COMMIT_TIME = "2026-08-07 13:52:04 +0530";
-export const BUILD_HASH = "0x8ac55f";
+export const BUILD_STEP = 61;
+export const COMMIT_MSG = "optimize helper code";
+export const COMMIT_TIME = "2026-08-07 13:56:25 +0530";
+export const BUILD_HASH = "0x569502";
