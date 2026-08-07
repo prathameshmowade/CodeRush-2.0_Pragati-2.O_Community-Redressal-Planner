6136824e9bf68bcd4cd5876ccc13cedb0d96527b
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 20:32:39 +0530

    reorganize icon code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 161;
-export const COMMIT_MSG = "optimize view speed";
-export const COMMIT_TIME = "2026-08-07 20:28:32 +0530";
-export const BUILD_HASH = "0x58a9fc";
+export const BUILD_STEP = 162;
+export const COMMIT_MSG = "reorganize icon code";
+export const COMMIT_TIME = "2026-08-07 20:32:39 +0530";
+export const BUILD_HASH = "0x306fe4";
