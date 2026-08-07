1c84e510552a3206d41897016deaecff9a502dc5
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 11:30:21 +0530

    align config logic

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 23;
-export const COMMIT_MSG = "improve dialog code";
-export const COMMIT_TIME = "2026-08-07 11:26:14 +0530";
-export const BUILD_HASH = "0x53437a";
+export const BUILD_STEP = 24;
+export const COMMIT_MSG = "align config logic";
+export const COMMIT_TIME = "2026-08-07 11:30:21 +0530";
+export const BUILD_HASH = "0x468bb0";
