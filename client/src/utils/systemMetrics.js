b2eb2a34a4830a90de811e37f3babcbc3b1230f8
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 16:41:16 +0530

    clean route ui

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 102;
-export const COMMIT_MSG = "format view ui";
-export const COMMIT_TIME = "2026-08-07 16:37:18 +0530";
-export const BUILD_HASH = "0x3e088e";
+export const BUILD_STEP = 103;
+export const COMMIT_MSG = "clean route ui";
+export const COMMIT_TIME = "2026-08-07 16:41:16 +0530";
+export const BUILD_HASH = "0x413a61";
