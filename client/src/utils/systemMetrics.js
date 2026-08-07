fd610b09e2c579d262928dcc7dc1a56ec5ad8469
Kanchan <kanchan874@users.noreply.github.com>
2026-08-08 00:31:06 +0530

    format picker speed

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,4 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 13;
-export const LAST_ACTION = "improve upload grid";
-export const SYNC_HASH = "0x9ed6f";
+export const BUILD_METRIC = 14;
+export const LAST_ACTION = "format picker speed";
+export const SYNC_HASH = "0x65ceb";
