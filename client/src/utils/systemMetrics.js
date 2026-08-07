821dafd5af3e0d98d897d425f0368695eb74db11
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 14:27:50 +0530

    enhance auth code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 68;
-export const COMMIT_MSG = "sync banner ui";
-export const COMMIT_TIME = "2026-08-07 14:23:41 +0530";
-export const BUILD_HASH = "0x5badae";
+export const BUILD_STEP = 69;
+export const COMMIT_MSG = "enhance auth code";
+export const COMMIT_TIME = "2026-08-07 14:27:50 +0530";
+export const BUILD_HASH = "0x395a59";
