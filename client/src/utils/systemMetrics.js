f93193648c18d33e5d3c0a00e17e66e14fac00c4
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 19:05:51 +0530

    add view types

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 139;
-export const COMMIT_MSG = "sync icon data";
-export const COMMIT_TIME = "2026-08-07 19:03:13 +0530";
-export const BUILD_HASH = "0x15aec8";
+export const BUILD_STEP = 140;
+export const COMMIT_MSG = "add view types";
+export const COMMIT_TIME = "2026-08-07 19:05:51 +0530";
+export const BUILD_HASH = "0x7fd9af";
