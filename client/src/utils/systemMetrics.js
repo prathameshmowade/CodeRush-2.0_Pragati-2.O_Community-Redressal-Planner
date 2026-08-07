e94477f668029494bdbb1a18934de05ebf72ca36
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 22:42:39 +0530

    tweak navbar speed

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 194;
-export const COMMIT_MSG = "update button data";
-export const COMMIT_TIME = "2026-08-07 22:39:22 +0530";
-export const BUILD_HASH = "0x63e11f";
+export const BUILD_STEP = 195;
+export const COMMIT_MSG = "tweak navbar speed";
+export const COMMIT_TIME = "2026-08-07 22:42:39 +0530";
+export const BUILD_HASH = "0x15595a";
