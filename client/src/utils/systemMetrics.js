613ef3f1690d9a90ae8be0e9997db998e6b4d208
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 11:22:35 +0530

    optimize hook data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 21;
-export const COMMIT_MSG = "sync form";
-export const COMMIT_TIME = "2026-08-07 11:18:45 +0530";
-export const BUILD_HASH = "0x60e0a0";
+export const BUILD_STEP = 22;
+export const COMMIT_MSG = "optimize hook data";
+export const COMMIT_TIME = "2026-08-07 11:22:35 +0530";
+export const BUILD_HASH = "0x2cd952";
