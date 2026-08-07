8b23950e7e4784477472d1cdcd0776545da4e754
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 10:23:56 +0530

    enhance card data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 6;
-export const COMMIT_MSG = "optimize audit logic";
-export const COMMIT_TIME = "2026-08-07 10:19:50 +0530";
-export const BUILD_HASH = "0x1307c1";
+export const BUILD_STEP = 7;
+export const COMMIT_MSG = "enhance card data";
+export const COMMIT_TIME = "2026-08-07 10:23:56 +0530";
+export const BUILD_HASH = "0x2d476f";
