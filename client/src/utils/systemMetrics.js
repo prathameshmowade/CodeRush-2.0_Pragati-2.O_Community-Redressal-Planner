88fa126e32bd3a9583a826a1dc19b7875e7c12bd
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 22:30:41 +0530

    fix badge

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 191;
-export const COMMIT_MSG = "align navbar ux";
-export const COMMIT_TIME = "2026-08-07 22:27:41 +0530";
-export const BUILD_HASH = "0x787797";
+export const BUILD_STEP = 192;
+export const COMMIT_MSG = "fix badge";
+export const COMMIT_TIME = "2026-08-07 22:30:41 +0530";
+export const BUILD_HASH = "0x85ea17";
