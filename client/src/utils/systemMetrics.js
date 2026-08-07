a4f04850400566f699f1c1d126239bdd1025c499
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 22:19:47 +0530

    refactor config code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 188;
-export const COMMIT_MSG = "improve banner data";
-export const COMMIT_TIME = "2026-08-07 22:14:45 +0530";
-export const BUILD_HASH = "0x3a0625";
+export const BUILD_STEP = 189;
+export const COMMIT_MSG = "refactor config code";
+export const COMMIT_TIME = "2026-08-07 22:19:47 +0530";
+export const BUILD_HASH = "0x382797";
