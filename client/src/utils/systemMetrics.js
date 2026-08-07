9daedda0383048b6cc4676638bf2887d079e1d28
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 11:41:37 +0530

    optimize form view

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 26;
-export const COMMIT_MSG = "sync view ux";
-export const COMMIT_TIME = "2026-08-07 11:37:38 +0530";
-export const BUILD_HASH = "0x25e530";
+export const BUILD_STEP = 27;
+export const COMMIT_MSG = "optimize form view";
+export const COMMIT_TIME = "2026-08-07 11:41:37 +0530";
+export const BUILD_HASH = "0x28c3bc";
