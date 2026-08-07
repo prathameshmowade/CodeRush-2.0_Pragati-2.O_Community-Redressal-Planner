020ac4c07a418b673bf8fa45199a22a8fefced8a
Neha Musale <NehaMusale11@users.noreply.github.com>
2026-08-08 00:31:04 +0530

    sync icon logic

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,4 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 1;
-export const LAST_ACTION = "format feed data";
-export const SYNC_HASH = "0xce221";
+export const BUILD_METRIC = 2;
+export const LAST_ACTION = "sync icon logic";
+export const SYNC_HASH = "0x2082c";
