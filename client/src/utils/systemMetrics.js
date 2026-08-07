e5e933123c8b2e2b5ec3ebdfdab2306c106e356a
Yash K <Yash-k10@users.noreply.github.com>
2026-08-08 00:31:06 +0530

    refactor view flow

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,4 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 17;
-export const LAST_ACTION = "adjust model flow";
-export const SYNC_HASH = "0x78369";
+export const BUILD_METRIC = 18;
+export const LAST_ACTION = "refactor view flow";
+export const SYNC_HASH = "0xe15c1";
