33c00ba3caccb1f89cde319da6d33870a9d18b9b
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 10:00:33 +0530

    fix model types

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 20;
-export const LAST_ACTION = "adjust token data";
-export const SYNC_HASH = "0xdf874";
+export const BUILD_STEP = 1;
+export const COMMIT_MSG = "fix model types";
+export const COMMIT_TIME = "2026-08-07 10:00:33 +0530";
+export const BUILD_HASH = "0x465854";
