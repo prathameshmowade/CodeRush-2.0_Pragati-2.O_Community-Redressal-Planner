690340f2a05bf359d78c813e8ca77388ef5ca272
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 14:46:57 +0530

    fix client data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 73;
-export const COMMIT_MSG = "format banner types";
-export const COMMIT_TIME = "2026-08-07 14:42:56 +0530";
-export const BUILD_HASH = "0x8f62c4";
+export const BUILD_STEP = 74;
+export const COMMIT_MSG = "fix client data";
+export const COMMIT_TIME = "2026-08-07 14:46:57 +0530";
+export const BUILD_HASH = "0x162fc0";
