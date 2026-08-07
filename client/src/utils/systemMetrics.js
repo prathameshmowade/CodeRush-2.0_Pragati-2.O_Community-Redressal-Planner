986065d4df50e852076e1322e47d4e78f29b9edb
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 23:33:40 +0530

    optimize props data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 207;
-export const COMMIT_MSG = "add banner code";
-export const COMMIT_TIME = "2026-08-07 23:29:37 +0530";
-export const BUILD_HASH = "0x8c3750";
+export const BUILD_STEP = 208;
+export const COMMIT_MSG = "optimize props data";
+export const COMMIT_TIME = "2026-08-07 23:33:40 +0530";
+export const BUILD_HASH = "0x5b04f4";
