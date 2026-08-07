e5ade85c9465ca1e859f73748ac4e1d02b5df724
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 22:14:45 +0530

    improve banner data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 187;
-export const COMMIT_MSG = "sync route types";
-export const COMMIT_TIME = "2026-08-07 22:10:51 +0530";
-export const BUILD_HASH = "0x653418";
+export const BUILD_STEP = 188;
+export const COMMIT_MSG = "improve banner data";
+export const COMMIT_TIME = "2026-08-07 22:14:45 +0530";
+export const BUILD_HASH = "0x3a0625";
