5aefdc360730b65b72f752adea71d7d3b60a15bb
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 17:55:10 +0530

    clean layout ui

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 121;
-export const COMMIT_MSG = "add config speed";
-export const COMMIT_TIME = "2026-08-07 17:51:57 +0530";
-export const BUILD_HASH = "0x7a397b";
+export const BUILD_STEP = 122;
+export const COMMIT_MSG = "clean layout ui";
+export const COMMIT_TIME = "2026-08-07 17:55:10 +0530";
+export const BUILD_HASH = "0x55c780";
