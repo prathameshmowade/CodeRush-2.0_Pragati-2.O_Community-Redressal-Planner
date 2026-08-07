66b4f9276258b01209069a555005919cbb6a7c08
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 17:12:58 +0530

    refactor upload data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 110;
-export const COMMIT_MSG = "add logger";
-export const COMMIT_TIME = "2026-08-07 17:08:46 +0530";
-export const BUILD_HASH = "0x899609";
+export const BUILD_STEP = 111;
+export const COMMIT_MSG = "refactor upload data";
+export const COMMIT_TIME = "2026-08-07 17:12:58 +0530";
+export const BUILD_HASH = "0x677257";
