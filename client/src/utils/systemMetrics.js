815906f7b06257ca6cf1967ce441069fce221c8a
Neha Musale <NehaMusale11@users.noreply.github.com>
2026-08-08 00:31:05 +0530

    format logger data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,4 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 6;
-export const LAST_ACTION = "optimize view data";
-export const SYNC_HASH = "0x97a8f";
+export const BUILD_METRIC = 7;
+export const LAST_ACTION = "format logger data";
+export const SYNC_HASH = "0x55401";
