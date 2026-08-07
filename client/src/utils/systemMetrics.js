531660f4ce9d9bd638373f4dd14652b4a69da9a8
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 17:27:42 +0530

    add props flow

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 114;
-export const COMMIT_MSG = "improve header code";
-export const COMMIT_TIME = "2026-08-07 17:24:34 +0530";
-export const BUILD_HASH = "0x910bfb";
+export const BUILD_STEP = 115;
+export const COMMIT_MSG = "add props flow";
+export const COMMIT_TIME = "2026-08-07 17:27:42 +0530";
+export const BUILD_HASH = "0x58f371";
