a2d47dc03efbbea5d843f3a26efb223818f34628
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 21:47:40 +0530

    clean badge code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 180;
-export const COMMIT_MSG = "add banner logic";
-export const COMMIT_TIME = "2026-08-07 21:43:47 +0530";
-export const BUILD_HASH = "0x8c262a";
+export const BUILD_STEP = 181;
+export const COMMIT_MSG = "clean badge code";
+export const COMMIT_TIME = "2026-08-07 21:47:40 +0530";
+export const BUILD_HASH = "0x8c3718";
