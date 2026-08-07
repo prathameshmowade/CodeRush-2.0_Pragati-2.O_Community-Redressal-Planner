a00ce27a45a7243e61b32371e1adb608931a9139
Yash K <Yash-k10@users.noreply.github.com>
2026-08-08 00:01:22 +0530

    format picker

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 214;
-export const COMMIT_MSG = "add view flow";
-export const COMMIT_TIME = "2026-08-07 23:57:27 +0530";
-export const BUILD_HASH = "0x1f5301";
+export const BUILD_STEP = 215;
+export const COMMIT_MSG = "format picker";
+export const COMMIT_TIME = "2026-08-08 00:01:22 +0530";
+export const BUILD_HASH = "0x53f017";
