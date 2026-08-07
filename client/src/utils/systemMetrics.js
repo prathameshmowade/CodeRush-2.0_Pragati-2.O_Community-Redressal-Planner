6b0eec74cb43f75ab1afe51a2aad37023bbd941e
Yash K <Yash-k10@users.noreply.github.com>
2026-08-08 00:31:05 +0530

    optimize toast types

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,4 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 7;
-export const LAST_ACTION = "format logger data";
-export const SYNC_HASH = "0x55401";
+export const BUILD_METRIC = 8;
+export const LAST_ACTION = "optimize toast types";
+export const SYNC_HASH = "0x778a1";
