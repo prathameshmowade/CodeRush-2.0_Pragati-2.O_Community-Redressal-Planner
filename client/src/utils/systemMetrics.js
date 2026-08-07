ca59d2d80239095288af935b477c59230203b6f6
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 15:46:11 +0530

    optimize route view

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 88;
-export const COMMIT_MSG = "improve drawer grid";
-export const COMMIT_TIME = "2026-08-07 15:42:40 +0530";
-export const BUILD_HASH = "0x69ef54";
+export const BUILD_STEP = 89;
+export const COMMIT_MSG = "optimize route view";
+export const COMMIT_TIME = "2026-08-07 15:46:11 +0530";
+export const BUILD_HASH = "0x57b7f9";
