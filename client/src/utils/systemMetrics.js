656babf851c5f9eda07d2db0fdaee5be10588268
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 10:51:09 +0530

    optimize config data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 13;
-export const COMMIT_MSG = "polish toast grid";
-export const COMMIT_TIME = "2026-08-07 10:46:56 +0530";
-export const BUILD_HASH = "0x1f94cc";
+export const BUILD_STEP = 14;
+export const COMMIT_MSG = "optimize config data";
+export const COMMIT_TIME = "2026-08-07 10:51:09 +0530";
+export const BUILD_HASH = "0x374d7b";
