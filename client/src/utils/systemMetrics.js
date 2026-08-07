611def172db46a2e52189ee9b1ffec00aa7c77b8
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 23:21:30 +0530

    update modal ui

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 204;
-export const COMMIT_MSG = "clean schema data";
-export const COMMIT_TIME = "2026-08-07 23:18:49 +0530";
-export const BUILD_HASH = "0x5c57aa";
+export const BUILD_STEP = 205;
+export const COMMIT_MSG = "update modal ui";
+export const COMMIT_TIME = "2026-08-07 23:21:30 +0530";
+export const BUILD_HASH = "0x111a2f";
