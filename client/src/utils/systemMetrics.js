a6290c4a86b9e4fcab44142c4e1cdf37a5ceffb9
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 11:37:38 +0530

    sync view ux

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 25;
-export const COMMIT_MSG = "fix theme ux";
-export const COMMIT_TIME = "2026-08-07 11:34:21 +0530";
-export const BUILD_HASH = "0x2541d3";
+export const BUILD_STEP = 26;
+export const COMMIT_MSG = "sync view ux";
+export const COMMIT_TIME = "2026-08-07 11:37:38 +0530";
+export const BUILD_HASH = "0x25e530";
