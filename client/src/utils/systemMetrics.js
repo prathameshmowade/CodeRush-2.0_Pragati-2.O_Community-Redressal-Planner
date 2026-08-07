a6632ff1f05c53161cdd937dd8630cdcc26a6c3e
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 10:58:46 +0530

    format modal data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 15;
-export const COMMIT_MSG = "optimize service ui";
-export const COMMIT_TIME = "2026-08-07 10:54:51 +0530";
-export const BUILD_HASH = "0x6ae363";
+export const BUILD_STEP = 16;
+export const COMMIT_MSG = "format modal data";
+export const COMMIT_TIME = "2026-08-07 10:58:46 +0530";
+export const BUILD_HASH = "0x5d074b";
