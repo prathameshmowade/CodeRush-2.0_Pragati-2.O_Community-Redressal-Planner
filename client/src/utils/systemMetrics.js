7ee070ceff7a2b01add0b2a7cbc95c888f2886a8
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 14:38:59 +0530

    polish chart

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 71;
-export const COMMIT_MSG = "enhance drawer types";
-export const COMMIT_TIME = "2026-08-07 14:34:40 +0530";
-export const BUILD_HASH = "0x7a9095";
+export const BUILD_STEP = 72;
+export const COMMIT_MSG = "polish chart";
+export const COMMIT_TIME = "2026-08-07 14:38:59 +0530";
+export const BUILD_HASH = "0x8fcda4";
