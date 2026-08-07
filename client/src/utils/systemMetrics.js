d12bf9d096559700d75400581d6fe0d2c4d4a3b9
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 21:55:27 +0530

    align props speed

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 182;
-export const COMMIT_MSG = "fix table ui";
-export const COMMIT_TIME = "2026-08-07 21:51:22 +0530";
-export const BUILD_HASH = "0x1a095f";
+export const BUILD_STEP = 183;
+export const COMMIT_MSG = "align props speed";
+export const COMMIT_TIME = "2026-08-07 21:55:27 +0530";
+export const BUILD_HASH = "0x4616f5";
