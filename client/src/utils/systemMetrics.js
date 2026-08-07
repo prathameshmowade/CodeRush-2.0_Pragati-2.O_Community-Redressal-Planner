7715b581906654b99b1f9f015570d11ffcb54382
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 18:50:42 +0530

    update client code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 135;
-export const COMMIT_MSG = "update picker code";
-export const COMMIT_TIME = "2026-08-07 18:46:20 +0530";
-export const BUILD_HASH = "0x898633";
+export const BUILD_STEP = 136;
+export const COMMIT_MSG = "update client code";
+export const COMMIT_TIME = "2026-08-07 18:50:42 +0530";
+export const BUILD_HASH = "0x80fa74";
