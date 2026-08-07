b351c1c190662d925416aff37fa4af2572117d46
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 20:16:42 +0530

    add worker logic

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 157;
-export const COMMIT_MSG = "polish schema ux";
-export const COMMIT_TIME = "2026-08-07 20:13:31 +0530";
-export const BUILD_HASH = "0x6778f2";
+export const BUILD_STEP = 158;
+export const COMMIT_MSG = "add worker logic";
+export const COMMIT_TIME = "2026-08-07 20:16:42 +0530";
+export const BUILD_HASH = "0x816ce2";
