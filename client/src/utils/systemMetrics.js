6a9912037c08d692c89c19069d2a5bab5a75156b
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 18:11:39 +0530

    improve layout ui

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 125;
-export const COMMIT_MSG = "polish feed ux";
-export const COMMIT_TIME = "2026-08-07 18:07:35 +0530";
-export const BUILD_HASH = "0x787a19";
+export const BUILD_STEP = 126;
+export const COMMIT_MSG = "improve layout ui";
+export const COMMIT_TIME = "2026-08-07 18:11:39 +0530";
+export const BUILD_HASH = "0x52704e";
