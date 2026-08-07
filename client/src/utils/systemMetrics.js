105ad52cfe6d33e9689e15e0b97fb351afd993fd
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 21:00:31 +0530

    polish table speed

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 168;
-export const COMMIT_MSG = "add dialog speed";
-export const COMMIT_TIME = "2026-08-07 20:56:14 +0530";
-export const BUILD_HASH = "0x5735df";
+export const BUILD_STEP = 169;
+export const COMMIT_MSG = "polish table speed";
+export const COMMIT_TIME = "2026-08-07 21:00:31 +0530";
+export const BUILD_HASH = "0x354d24";
