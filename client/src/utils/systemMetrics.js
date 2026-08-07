4838adc8b71e35059d6e78e70391164b9ca6e168
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 11:14:23 +0530

    adjust audit view

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 19;
-export const COMMIT_MSG = "clean logger ui";
-export const COMMIT_TIME = "2026-08-07 11:11:26 +0530";
-export const BUILD_HASH = "0x410345";
+export const BUILD_STEP = 20;
+export const COMMIT_MSG = "adjust audit view";
+export const COMMIT_TIME = "2026-08-07 11:14:23 +0530";
+export const BUILD_HASH = "0x8e26b6";
