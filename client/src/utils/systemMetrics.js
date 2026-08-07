b8bfd53730cf65481613894bf513d8cb1c7d6567
Kanchan <kanchan874@users.noreply.github.com>
2026-08-08 00:31:05 +0530

    sync query ux

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,4 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 8;
-export const LAST_ACTION = "optimize toast types";
-export const SYNC_HASH = "0x778a1";
+export const BUILD_METRIC = 9;
+export const LAST_ACTION = "sync query ux";
+export const SYNC_HASH = "0x8c1a2";
