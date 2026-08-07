3128939a758a523bba8b989ad21f8fd555262710
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 17:04:38 +0530

    fix dialog types

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 108;
-export const COMMIT_MSG = "improve banner";
-export const COMMIT_TIME = "2026-08-07 17:01:11 +0530";
-export const BUILD_HASH = "0x6cee84";
+export const BUILD_STEP = 109;
+export const COMMIT_MSG = "fix dialog types";
+export const COMMIT_TIME = "2026-08-07 17:04:38 +0530";
+export const BUILD_HASH = "0x29b2d5";
