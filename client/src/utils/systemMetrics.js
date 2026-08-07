5e148d696ed895c5120ee7e0ee5b4fbb40fb2391
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 23:53:15 +0530

    align query types

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 212;
-export const COMMIT_MSG = "enhance drawer speed";
-export const COMMIT_TIME = "2026-08-07 23:49:05 +0530";
-export const BUILD_HASH = "0x113621";
+export const BUILD_STEP = 213;
+export const COMMIT_MSG = "align query types";
+export const COMMIT_TIME = "2026-08-07 23:53:15 +0530";
+export const BUILD_HASH = "0x279f65";
