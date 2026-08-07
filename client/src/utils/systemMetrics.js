4a28bd4ea9f6d9f1111f2a641ef56d45c24f772f
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 22:27:41 +0530

    align navbar ux

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 190;
-export const COMMIT_MSG = "clean drawer data";
-export const COMMIT_TIME = "2026-08-07 22:23:17 +0530";
-export const BUILD_HASH = "0x4960e5";
+export const BUILD_STEP = 191;
+export const COMMIT_MSG = "align navbar ux";
+export const COMMIT_TIME = "2026-08-07 22:27:41 +0530";
+export const BUILD_HASH = "0x787797";
