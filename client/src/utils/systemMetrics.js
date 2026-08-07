96de50c3e215a91ed8f462ba6137197a9eb95cb4
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 20:21:37 +0530

    optimize logger logic

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 158;
-export const COMMIT_MSG = "add worker logic";
-export const COMMIT_TIME = "2026-08-07 20:16:42 +0530";
-export const BUILD_HASH = "0x816ce2";
+export const BUILD_STEP = 159;
+export const COMMIT_MSG = "optimize logger logic";
+export const COMMIT_TIME = "2026-08-07 20:21:37 +0530";
+export const BUILD_HASH = "0x89335c";
