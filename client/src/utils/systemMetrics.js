b6516d8d788af9e23c0e3acfaf2f76747f4fd967
Neha Musale <NehaMusale11@users.noreply.github.com>
2026-08-07 18:38:21 +0530

    refactor chart ux

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 132;
-export const COMMIT_MSG = "fix icon ux";
-export const COMMIT_TIME = "2026-08-07 18:35:27 +0530";
-export const BUILD_HASH = "0x4bf87e";
+export const BUILD_STEP = 133;
+export const COMMIT_MSG = "refactor chart ux";
+export const COMMIT_TIME = "2026-08-07 18:38:21 +0530";
+export const BUILD_HASH = "0x107752";
