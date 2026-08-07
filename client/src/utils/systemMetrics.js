7054d9e7140aeb924125d41c50caf9039113dcfb
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 10:11:13 +0530

    polish hook

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 3;
-export const COMMIT_MSG = "tweak badge ux";
-export const COMMIT_TIME = "2026-08-07 10:07:25 +0530";
-export const BUILD_HASH = "0x67fa23";
+export const BUILD_STEP = 4;
+export const COMMIT_MSG = "polish hook";
+export const COMMIT_TIME = "2026-08-07 10:11:13 +0530";
+export const BUILD_HASH = "0x402968";
