627bca9b3f77661291a4ed31e30dd1b92ddd240d
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 13:24:01 +0530

    align theme code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 52;
-export const COMMIT_MSG = "fix navbar code";
-export const COMMIT_TIME = "2026-08-07 13:20:58 +0530";
-export const BUILD_HASH = "0x322007";
+export const BUILD_STEP = 53;
+export const COMMIT_MSG = "align theme code";
+export const COMMIT_TIME = "2026-08-07 13:24:01 +0530";
+export const BUILD_HASH = "0x5e5691";
