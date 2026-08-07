502276812949744c15af8bd615f160e728c8cddd
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 20:13:31 +0530

    polish schema ux

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 156;
-export const COMMIT_MSG = "format props code";
-export const COMMIT_TIME = "2026-08-07 20:09:01 +0530";
-export const BUILD_HASH = "0x61bf4c";
+export const BUILD_STEP = 157;
+export const COMMIT_MSG = "polish schema ux";
+export const COMMIT_TIME = "2026-08-07 20:13:31 +0530";
+export const BUILD_HASH = "0x6778f2";
