f92afab75bd180e2bca7ce571d2a95a95621461a
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 16:56:51 +0530

    enhance picker speed

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 106;
-export const COMMIT_MSG = "fix footer ui";
-export const COMMIT_TIME = "2026-08-07 16:52:39 +0530";
-export const BUILD_HASH = "0xfe876";
+export const BUILD_STEP = 107;
+export const COMMIT_MSG = "enhance picker speed";
+export const COMMIT_TIME = "2026-08-07 16:56:51 +0530";
+export const BUILD_HASH = "0x12f9bd";
