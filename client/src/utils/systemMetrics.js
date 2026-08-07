5de346bf9baf89b1a19acfd636ad173a86c4859e
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-08 00:09:56 +0530

    format badge flow

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 216;
-export const COMMIT_MSG = "patch footer data";
-export const COMMIT_TIME = "2026-08-08 00:05:39 +0530";
-export const BUILD_HASH = "0x2ac71b";
+export const BUILD_STEP = 217;
+export const COMMIT_MSG = "format badge flow";
+export const COMMIT_TIME = "2026-08-08 00:09:56 +0530";
+export const BUILD_HASH = "0x50355b";
