aa4b16fdfd9e59c4aa7e378041bfa846cf4c8333
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 18:42:48 +0530

    tweak badge

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 133;
-export const COMMIT_MSG = "refactor chart ux";
-export const COMMIT_TIME = "2026-08-07 18:38:21 +0530";
-export const BUILD_HASH = "0x107752";
+export const BUILD_STEP = 134;
+export const COMMIT_MSG = "tweak badge";
+export const COMMIT_TIME = "2026-08-07 18:42:48 +0530";
+export const BUILD_HASH = "0x558f51";
