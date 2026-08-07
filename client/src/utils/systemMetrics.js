1acfdf650be22025af324595c52fa61ec865283b
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 10:07:25 +0530

    tweak badge ux

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 2;
-export const COMMIT_MSG = "add state code";
-export const COMMIT_TIME = "2026-08-07 10:04:30 +0530";
-export const BUILD_HASH = "0x7dae3a";
+export const BUILD_STEP = 3;
+export const COMMIT_MSG = "tweak badge ux";
+export const COMMIT_TIME = "2026-08-07 10:07:25 +0530";
+export const BUILD_HASH = "0x67fa23";
