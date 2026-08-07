ab423c90caea1fdd1bb5f927f242e71b1eca5a0c
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 23:57:27 +0530

    add view flow

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 213;
-export const COMMIT_MSG = "align query types";
-export const COMMIT_TIME = "2026-08-07 23:53:15 +0530";
-export const BUILD_HASH = "0x279f65";
+export const BUILD_STEP = 214;
+export const COMMIT_MSG = "add view flow";
+export const COMMIT_TIME = "2026-08-07 23:57:27 +0530";
+export const BUILD_HASH = "0x1f5301";
