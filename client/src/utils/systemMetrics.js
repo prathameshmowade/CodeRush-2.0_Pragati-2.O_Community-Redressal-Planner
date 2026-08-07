379c1585a0179c1925cde0be0a886fe2e2b6497b
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 23:25:25 +0530

    optimize button data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 205;
-export const COMMIT_MSG = "update modal ui";
-export const COMMIT_TIME = "2026-08-07 23:21:30 +0530";
-export const BUILD_HASH = "0x111a2f";
+export const BUILD_STEP = 206;
+export const COMMIT_MSG = "optimize button data";
+export const COMMIT_TIME = "2026-08-07 23:25:25 +0530";
+export const BUILD_HASH = "0x55614d";
