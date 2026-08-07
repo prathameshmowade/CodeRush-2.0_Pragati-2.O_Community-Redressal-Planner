a430c15b514b3b9e63d6e4c76f4747870b837cd5
Yash K <Yash-k10@users.noreply.github.com>
2026-08-08 00:31:05 +0530

    improve upload grid

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,4 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 12;
-export const LAST_ACTION = "adjust feed ux";
-export const SYNC_HASH = "0x4fbef";
+export const BUILD_METRIC = 13;
+export const LAST_ACTION = "improve upload grid";
+export const SYNC_HASH = "0x9ed6f";
