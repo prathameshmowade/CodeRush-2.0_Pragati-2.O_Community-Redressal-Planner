7d042c5abd981063f4f093b989179d5677120aa9
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 10:30:57 +0530

    fix table

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 8;
-export const COMMIT_MSG = "tweak card flow";
-export const COMMIT_TIME = "2026-08-07 10:27:33 +0530";
-export const BUILD_HASH = "0x88c218";
+export const BUILD_STEP = 9;
+export const COMMIT_MSG = "fix table";
+export const COMMIT_TIME = "2026-08-07 10:30:57 +0530";
+export const BUILD_HASH = "0x971444";
