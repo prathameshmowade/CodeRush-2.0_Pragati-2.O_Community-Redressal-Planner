bd5a2f4ab9af6fc5d371fb46454cd98ca8b4d13b
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 21:12:54 +0530

    add picker code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 171;
-export const COMMIT_MSG = "fix table ux";
-export const COMMIT_TIME = "2026-08-07 21:09:02 +0530";
-export const BUILD_HASH = "0x1cf54d";
+export const BUILD_STEP = 172;
+export const COMMIT_MSG = "add picker code";
+export const COMMIT_TIME = "2026-08-07 21:12:54 +0530";
+export const BUILD_HASH = "0x965f79";
