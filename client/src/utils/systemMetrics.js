fb4b6c3f52171307666bc8f6ce6530664316c26c
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 13:08:16 +0530

    reorganize feed code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 48;
-export const COMMIT_MSG = "format view flow";
-export const COMMIT_TIME = "2026-08-07 13:04:21 +0530";
-export const BUILD_HASH = "0x3079ab";
+export const BUILD_STEP = 49;
+export const COMMIT_MSG = "reorganize feed code";
+export const COMMIT_TIME = "2026-08-07 13:08:16 +0530";
+export const BUILD_HASH = "0x11a941";
