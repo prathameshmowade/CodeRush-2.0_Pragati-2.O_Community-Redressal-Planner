c3fb5873014878a6c59ef3b848794d866ec3ebe2
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 19:45:16 +0530

    clean icon types

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 149;
-export const COMMIT_MSG = "clean dialog view";
-export const COMMIT_TIME = "2026-08-07 19:42:17 +0530";
-export const BUILD_HASH = "0x810838";
+export const BUILD_STEP = 150;
+export const COMMIT_MSG = "clean icon types";
+export const COMMIT_TIME = "2026-08-07 19:45:16 +0530";
+export const BUILD_HASH = "0x1cce0a";
