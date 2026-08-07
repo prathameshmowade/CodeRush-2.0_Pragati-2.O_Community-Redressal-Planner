74f4e6722e0f91709d708e65574a1ce9af51e245
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-08 00:31:04 +0530

    add audit ux

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,4 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 4;
-export const LAST_ACTION = "polish card view";
-export const SYNC_HASH = "0x825d5";
+export const BUILD_METRIC = 5;
+export const LAST_ACTION = "add audit ux";
+export const SYNC_HASH = "0x2ea26";
