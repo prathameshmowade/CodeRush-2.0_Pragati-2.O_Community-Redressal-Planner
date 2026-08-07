bbf4667fe4881c8cd904ebf0b4873d24d1e06f32
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 19:29:58 +0530

    reorganize picker ui

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 145;
-export const COMMIT_MSG = "tweak worker grid";
-export const COMMIT_TIME = "2026-08-07 19:26:38 +0530";
-export const BUILD_HASH = "0x5f56a1";
+export const BUILD_STEP = 146;
+export const COMMIT_MSG = "reorganize picker ui";
+export const COMMIT_TIME = "2026-08-07 19:29:58 +0530";
+export const BUILD_HASH = "0x8621f0";
