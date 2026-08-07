9b7c98e2722ed463c523ee4d2c82e841e662b888
Kanchan <kanchan874@users.noreply.github.com>
2026-08-08 00:31:04 +0530

    polish card view

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,4 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 3;
-export const LAST_ACTION = "adjust navbar grid";
-export const SYNC_HASH = "0x5dead";
+export const BUILD_METRIC = 4;
+export const LAST_ACTION = "polish card view";
+export const SYNC_HASH = "0x825d5";
