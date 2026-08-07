08d629942bcf419573bb2729cd9ee1ae99311429
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 15:38:37 +0530

    adjust card view

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 86;
-export const COMMIT_MSG = "add chart speed";
-export const COMMIT_TIME = "2026-08-07 15:34:45 +0530";
-export const BUILD_HASH = "0x1cc4e2";
+export const BUILD_STEP = 87;
+export const COMMIT_MSG = "adjust card view";
+export const COMMIT_TIME = "2026-08-07 15:38:37 +0530";
+export const BUILD_HASH = "0x104af7";
