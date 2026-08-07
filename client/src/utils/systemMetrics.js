96cac275b9df42755a4a0199226ea2dc2ab3faa1
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 19:57:53 +0530

    enhance button code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 152;
-export const COMMIT_MSG = "clean badge ui";
-export const COMMIT_TIME = "2026-08-07 19:53:50 +0530";
-export const BUILD_HASH = "0x8455b1";
+export const BUILD_STEP = 153;
+export const COMMIT_MSG = "enhance button code";
+export const COMMIT_TIME = "2026-08-07 19:57:53 +0530";
+export const BUILD_HASH = "0x488d16";
