b8bce936a1423529c744d30e14c5bb28c85ad2ed
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-08 00:31:05 +0530

    optimize view data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,4 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 5;
-export const LAST_ACTION = "add audit ux";
-export const SYNC_HASH = "0x2ea26";
+export const BUILD_METRIC = 6;
+export const LAST_ACTION = "optimize view data";
+export const SYNC_HASH = "0x97a8f";
