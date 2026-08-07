759e6d2f7dac9ef05b7e64d58219bb119ea65831
Neha Musale <NehaMusale11@users.noreply.github.com>
2026-08-07 11:58:27 +0530

    refactor upload ux

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 30;
-export const COMMIT_MSG = "patch props data";
-export const COMMIT_TIME = "2026-08-07 11:53:29 +0530";
-export const BUILD_HASH = "0x95dda5";
+export const BUILD_STEP = 31;
+export const COMMIT_MSG = "refactor upload ux";
+export const COMMIT_TIME = "2026-08-07 11:58:27 +0530";
+export const BUILD_HASH = "0x74cdbe";
