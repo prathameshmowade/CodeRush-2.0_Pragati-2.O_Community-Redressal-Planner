f5c6ecbe1c838571d5772d8cadc0865d25ec6ac2
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 14:50:27 +0530

    patch drawer code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 74;
-export const COMMIT_MSG = "fix client data";
-export const COMMIT_TIME = "2026-08-07 14:46:57 +0530";
-export const BUILD_HASH = "0x162fc0";
+export const BUILD_STEP = 75;
+export const COMMIT_MSG = "patch drawer code";
+export const COMMIT_TIME = "2026-08-07 14:50:27 +0530";
+export const BUILD_HASH = "0x10a2b2";
