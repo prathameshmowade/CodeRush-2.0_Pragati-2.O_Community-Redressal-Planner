62bdf831c5e7e07c92a556cd65a7dcd1c9388698
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 10:16:16 +0530

    polish modal code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 4;
-export const COMMIT_MSG = "polish hook";
-export const COMMIT_TIME = "2026-08-07 10:11:13 +0530";
-export const BUILD_HASH = "0x402968";
+export const BUILD_STEP = 5;
+export const COMMIT_MSG = "polish modal code";
+export const COMMIT_TIME = "2026-08-07 10:16:16 +0530";
+export const BUILD_HASH = "0x6be2e9";
