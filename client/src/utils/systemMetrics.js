98f62c1d537054b662be6d8b94b4a869bb4ec177
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 11:53:29 +0530

    patch props data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 29;
-export const COMMIT_MSG = "format audit";
-export const COMMIT_TIME = "2026-08-07 11:50:22 +0530";
-export const BUILD_HASH = "0x8b218a";
+export const BUILD_STEP = 30;
+export const COMMIT_MSG = "patch props data";
+export const COMMIT_TIME = "2026-08-07 11:53:29 +0530";
+export const BUILD_HASH = "0x95dda5";
