f9d20cb8e60d431b32dc99b45bacd18c91e2ddb7
Neha Musale <NehaMusale11@users.noreply.github.com>
2026-08-07 22:54:53 +0530

    update auth types

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 197;
-export const COMMIT_MSG = "patch schema logic";
-export const COMMIT_TIME = "2026-08-07 22:51:00 +0530";
-export const BUILD_HASH = "0x75daac";
+export const BUILD_STEP = 198;
+export const COMMIT_MSG = "update auth types";
+export const COMMIT_TIME = "2026-08-07 22:54:53 +0530";
+export const BUILD_HASH = "0x4bc476";
