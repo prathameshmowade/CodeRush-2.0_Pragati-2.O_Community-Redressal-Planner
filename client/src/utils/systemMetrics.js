928889c144577d84747aea0e510dacfe476b47bb
Neha Musale <NehaMusale11@users.noreply.github.com>
2026-08-07 11:11:26 +0530

    clean logger ui

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 18;
-export const COMMIT_MSG = "update icon";
-export const COMMIT_TIME = "2026-08-07 11:06:16 +0530";
-export const BUILD_HASH = "0x785e87";
+export const BUILD_STEP = 19;
+export const COMMIT_MSG = "clean logger ui";
+export const COMMIT_TIME = "2026-08-07 11:11:26 +0530";
+export const BUILD_HASH = "0x410345";
