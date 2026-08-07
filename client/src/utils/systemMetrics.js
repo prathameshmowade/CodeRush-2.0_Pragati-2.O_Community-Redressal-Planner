25cdad3673b9a0f0f88d7abf6076d30c83bda77e
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 14:19:00 +0530

    optimize dialog ux

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 66;
-export const COMMIT_MSG = "improve card types";
-export const COMMIT_TIME = "2026-08-07 14:15:43 +0530";
-export const BUILD_HASH = "0x84ff42";
+export const BUILD_STEP = 67;
+export const COMMIT_MSG = "optimize dialog ux";
+export const COMMIT_TIME = "2026-08-07 14:19:00 +0530";
+export const BUILD_HASH = "0x5a2b41";
