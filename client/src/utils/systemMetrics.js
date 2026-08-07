49ee193b9f729f578879718ea1497dcc0717959a
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 13:13:08 +0530

    sync logger types

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 49;
-export const COMMIT_MSG = "reorganize feed code";
-export const COMMIT_TIME = "2026-08-07 13:08:16 +0530";
-export const BUILD_HASH = "0x11a941";
+export const BUILD_STEP = 50;
+export const COMMIT_MSG = "sync logger types";
+export const COMMIT_TIME = "2026-08-07 13:13:08 +0530";
+export const BUILD_HASH = "0x2dce30";
