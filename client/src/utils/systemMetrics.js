1307916a0e3d808c4bc067ba805e9dbd01354a20
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 20:36:26 +0530

    improve theme ui

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 162;
-export const COMMIT_MSG = "reorganize icon code";
-export const COMMIT_TIME = "2026-08-07 20:32:39 +0530";
-export const BUILD_HASH = "0x306fe4";
+export const BUILD_STEP = 163;
+export const COMMIT_MSG = "improve theme ui";
+export const COMMIT_TIME = "2026-08-07 20:36:26 +0530";
+export const BUILD_HASH = "0x769b0f";
