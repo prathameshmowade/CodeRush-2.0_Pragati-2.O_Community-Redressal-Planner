4660326d21e2b7aba151e7681c813b4d335f58f9
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 21:59:33 +0530

    clean token ux

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 183;
-export const COMMIT_MSG = "align props speed";
-export const COMMIT_TIME = "2026-08-07 21:55:27 +0530";
-export const BUILD_HASH = "0x4616f5";
+export const BUILD_STEP = 184;
+export const COMMIT_MSG = "clean token ux";
+export const COMMIT_TIME = "2026-08-07 21:59:33 +0530";
+export const BUILD_HASH = "0x115343";
