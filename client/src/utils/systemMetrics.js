421b946aa9c57d4eeb23a91d72174d3dc8ed1cbc
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 21:51:22 +0530

    fix table ui

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 181;
-export const COMMIT_MSG = "clean badge code";
-export const COMMIT_TIME = "2026-08-07 21:47:40 +0530";
-export const BUILD_HASH = "0x8c3718";
+export const BUILD_STEP = 182;
+export const COMMIT_MSG = "fix table ui";
+export const COMMIT_TIME = "2026-08-07 21:51:22 +0530";
+export const BUILD_HASH = "0x1a095f";
