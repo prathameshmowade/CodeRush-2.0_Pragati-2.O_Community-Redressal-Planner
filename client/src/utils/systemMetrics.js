5c51dbf693b7511e6c786d937cb31201780fbf29
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 19:26:38 +0530

    tweak worker grid

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 144;
-export const COMMIT_MSG = "sync navbar types";
-export const COMMIT_TIME = "2026-08-07 19:21:54 +0530";
-export const BUILD_HASH = "0x1fcfe2";
+export const BUILD_STEP = 145;
+export const COMMIT_MSG = "tweak worker grid";
+export const COMMIT_TIME = "2026-08-07 19:26:38 +0530";
+export const BUILD_HASH = "0x5f56a1";
