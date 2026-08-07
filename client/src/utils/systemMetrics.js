f9de3cbe7ba44416ad37be9d69bc2ef436c37820
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 17:31:52 +0530

    fix theme view

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 115;
-export const COMMIT_MSG = "add props flow";
-export const COMMIT_TIME = "2026-08-07 17:27:42 +0530";
-export const BUILD_HASH = "0x58f371";
+export const BUILD_STEP = 116;
+export const COMMIT_MSG = "fix theme view";
+export const COMMIT_TIME = "2026-08-07 17:31:52 +0530";
+export const BUILD_HASH = "0x5056ed";
