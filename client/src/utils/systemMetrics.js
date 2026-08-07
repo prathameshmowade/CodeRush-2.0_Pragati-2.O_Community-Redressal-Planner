ab14350a510fed6e825482333cde21d42b14aaf2
Neha Musale <NehaMusale11@users.noreply.github.com>
2026-08-07 11:06:16 +0530

    update icon

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 17;
-export const COMMIT_MSG = "clean modal ui";
-export const COMMIT_TIME = "2026-08-07 11:03:14 +0530";
-export const BUILD_HASH = "0x51e7d0";
+export const BUILD_STEP = 18;
+export const COMMIT_MSG = "update icon";
+export const COMMIT_TIME = "2026-08-07 11:06:16 +0530";
+export const BUILD_HASH = "0x785e87";
