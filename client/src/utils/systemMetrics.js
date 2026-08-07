2e33c8879e97dc9fc77cfcbb1ff0fc3b6b62459a
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 17:47:33 +0530

    enhance logger view

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 119;
-export const COMMIT_MSG = "update style code";
-export const COMMIT_TIME = "2026-08-07 17:44:29 +0530";
-export const BUILD_HASH = "0x2055d8";
+export const BUILD_STEP = 120;
+export const COMMIT_MSG = "enhance logger view";
+export const COMMIT_TIME = "2026-08-07 17:47:33 +0530";
+export const BUILD_HASH = "0x604be1";
