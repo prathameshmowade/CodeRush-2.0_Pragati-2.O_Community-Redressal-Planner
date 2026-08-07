320543fe32cedfce8f29ebf2a5c38dec9fe10eec
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 10:43:16 +0530

    update form code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 11;
-export const COMMIT_MSG = "patch form types";
-export const COMMIT_TIME = "2026-08-07 10:39:36 +0530";
-export const BUILD_HASH = "0x16f794";
+export const BUILD_STEP = 12;
+export const COMMIT_MSG = "update form code";
+export const COMMIT_TIME = "2026-08-07 10:43:16 +0530";
+export const BUILD_HASH = "0x302d91";
