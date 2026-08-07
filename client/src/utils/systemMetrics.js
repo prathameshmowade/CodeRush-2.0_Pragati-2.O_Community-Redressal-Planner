3cec3c31e033c8cbb70bde65d183b9bf32ecd3c7
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 16:49:34 +0530

    refactor toast data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 104;
-export const COMMIT_MSG = "adjust auth code";
-export const COMMIT_TIME = "2026-08-07 16:44:24 +0530";
-export const BUILD_HASH = "0x537ce3";
+export const BUILD_STEP = 105;
+export const COMMIT_MSG = "refactor toast data";
+export const COMMIT_TIME = "2026-08-07 16:49:34 +0530";
+export const BUILD_HASH = "0x441e86";
