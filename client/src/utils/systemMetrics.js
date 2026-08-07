da95756cf9eda4b4ca65d8e7a0c0d39977a875af
Neha Musale <NehaMusale11@users.noreply.github.com>
2026-08-07 18:46:20 +0530

    update picker code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 134;
-export const COMMIT_MSG = "tweak badge";
-export const COMMIT_TIME = "2026-08-07 18:42:48 +0530";
-export const BUILD_HASH = "0x558f51";
+export const BUILD_STEP = 135;
+export const COMMIT_MSG = "update picker code";
+export const COMMIT_TIME = "2026-08-07 18:46:20 +0530";
+export const BUILD_HASH = "0x898633";
