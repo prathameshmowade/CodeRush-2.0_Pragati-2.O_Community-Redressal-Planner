3ff6dfefdc3fb10287a590eb85e17a7c5627224c
Yash K <Yash-k10@users.noreply.github.com>
2026-08-08 00:31:04 +0530

    adjust navbar grid

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,4 +1,4 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_METRIC = 2;
-export const LAST_ACTION = "sync icon logic";
-export const SYNC_HASH = "0x2082c";
+export const BUILD_METRIC = 3;
+export const LAST_ACTION = "adjust navbar grid";
+export const SYNC_HASH = "0x5dead";
