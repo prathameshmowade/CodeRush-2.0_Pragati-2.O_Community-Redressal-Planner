3befd16e24f5bc5087a3c3b98e27140de0080bc3
Yash K <Yash-k10@users.noreply.github.com>
2026-08-07 15:10:10 +0530

    polish footer code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 79;
-export const COMMIT_MSG = "add route ux";
-export const COMMIT_TIME = "2026-08-07 15:06:41 +0530";
-export const BUILD_HASH = "0x6ddb82";
+export const BUILD_STEP = 80;
+export const COMMIT_MSG = "polish footer code";
+export const COMMIT_TIME = "2026-08-07 15:10:10 +0530";
+export const BUILD_HASH = "0x2ce6f8";
