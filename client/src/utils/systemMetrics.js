d32460195dec5eaac6e1fa767dd2de7e1aeb233d
Dhanshree <Dhanshree010@users.noreply.github.com>
2026-08-07 16:52:39 +0530

    fix footer ui

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 105;
-export const COMMIT_MSG = "refactor toast data";
-export const COMMIT_TIME = "2026-08-07 16:49:34 +0530";
-export const BUILD_HASH = "0x441e86";
+export const BUILD_STEP = 106;
+export const COMMIT_MSG = "fix footer ui";
+export const COMMIT_TIME = "2026-08-07 16:52:39 +0530";
+export const BUILD_HASH = "0xfe876";
