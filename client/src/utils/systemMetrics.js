a1cd516dd4aa6f86200aa46addb48b2a4bccbf9f
Prathamesh Mowade <prathameshmowade@users.noreply.github.com>
2026-08-07 19:03:13 +0530

    sync icon data

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 138;
-export const COMMIT_MSG = "add picker speed";
-export const COMMIT_TIME = "2026-08-07 18:58:55 +0530";
-export const BUILD_HASH = "0x813181";
+export const BUILD_STEP = 139;
+export const COMMIT_MSG = "sync icon data";
+export const COMMIT_TIME = "2026-08-07 19:03:13 +0530";
+export const BUILD_HASH = "0x15aec8";
