a4ef97a806106d71743316e37b680961cd0f8888
Kanchan <kanchan874@users.noreply.github.com>
2026-08-07 11:26:14 +0530

    improve dialog code

--- client/src/utils/systemMetrics.js
+++ client/src/utils/systemMetrics.js
@@ -1,5 +1,5 @@
 // Awaaz AI Build Sync Metric
-export const BUILD_STEP = 22;
-export const COMMIT_MSG = "optimize hook data";
-export const COMMIT_TIME = "2026-08-07 11:22:35 +0530";
-export const BUILD_HASH = "0x2cd952";
+export const BUILD_STEP = 23;
+export const COMMIT_MSG = "improve dialog code";
+export const COMMIT_TIME = "2026-08-07 11:26:14 +0530";
+export const BUILD_HASH = "0x53437a";
